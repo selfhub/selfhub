d818f9cc54fc6f99fc3411fe011ad5c202dddf1c
owen-d <ow.diehl@gmail.com>
2014-12-18 12:17:19 -0800

    basic server architecture

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -3,8 +3,21 @@ var bodyParser = require('body-parser');
 var helpers = require('./helpers.js');
 
 module.exports = function(app, express) {
+  var userRouter = express.Router();
+  var schemaRouter = express.Router();
+
+  //morgan for logging, bodyparser for...parsing body
+  //express Static for serving client directory
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
+
+  //error handling/logging
+  app.use(helpers.errorLogger);
+  app.use(helpers.errorHandler);
+
+  //inject routers into route files
+  require('../users/userRoutes')(userRouter);
+  require('../schemas/schemasRoutes')(schemaRouter);
 };
