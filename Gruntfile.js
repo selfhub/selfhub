d3c95cb1c82bcbc593f43a1fe3bfb069bafc10db
zindlerb <zindlerb@gmail.com>
2014-12-18 16:23:59 -0600

    added back in the tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,5 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
   grunt.loadNpmTasks('grunt-browserify');
 
-  grunt.registerTask('default', ['browserify']);
+  grunt.registerTask('default', ['jshint', 'jscs','browserify']);
 };
-//, 'jshint', 'jscs'
