1ac645c4e06deaaedac2bb383621fcbb74b276a6
owen-d <ow.diehl@gmail.com>
2014-12-18 16:54:45 -0800

    added mongo server models and mongoose connectivity

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
+
+var mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI);
 
 require('./config/middleware.js')(app, express);
 
