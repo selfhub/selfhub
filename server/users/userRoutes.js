3e1dcb16a57202063f59dcb6ebb18ef39b1a38f9
owen-d <ow.diehl@gmail.com>
2014-12-19 20:16:54 -0800

    signup/signin working, lacks token expiration. checkAuth function not done, needs client side communication.

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,2 +1,7 @@
+var userController = require('./userController');
+
 //we inject a router as the first argument (see: ../config/middleware)
-module.exports = function(router) {};
+module.exports = function(router) {
+  router.post('/signin', userController.signin);
+  router.post('/signup', userController.signup);
+};
