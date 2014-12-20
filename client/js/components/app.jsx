3533bc1f86abc6167ff7854348bae6cc5a2ea71d
Christian Perez <christian.manuel.perez@gmail.com>
2014-12-20 15:50:00 -0800

    Begin work on UI; get search filter to work

--- client/js/components/app.jsx
+++ client/js/components/app.jsx
@@ -1,10 +1,22 @@
 var React = require("react");
+var Navbar = require("./navbar.jsx");
+var Search = require("./search.jsx");
+
+// TODO: replace test data with actual data
+var schemas = [
+  { name: "Fitbit", route: "#/"},
+  { name: "Apple Healthkit", route: "#/"},
+  { name: "Nike Fuelband", route: "#/"}
+];
 
 var App = React.createClass({
   render: function() {
     return (
-      <div id="wrapper">
-        <h1>SelfHub</h1>
+      <div>
+        <Navbar />
+        <div className={"wrapper"}>
+          <Search items={schemas} />
+        </div>
       </div>
     );
   }
--- client/js/components/search.jsx
+++ client/js/components/search.jsx
@@ -1,10 +1,33 @@
-/** @jsx React.DOM */
 var React = require("react");
 
 var Search = React.createClass({
+  getInitialState: function() {
+    return {searchString: ""};
+  },
+  handleChange: function(e) {
+    this.setState({searchString: e.target.value});
+  },
   render: function() {
+    var schemas = this.props.items;
+    var searchString = this.state.searchString.trim().toLowerCase();
+    // filter the results
+    if (searchString.length > 0) {
+      schemas = schemas.filter(function(el) {
+        return el.name.toLowerCase().match(searchString);
+      });
+    }
     return (
-      <input type="text" placeholder="search for a schema" />
+      <div className={"search-block"}>  
+        <input className={"main-search"} type="text" value={this.state.searchString}
+               onChange={this.handleChange} placeholder="search for a schema" />
+        <ul className={"search-results"}>
+          {
+            schemas.map(function(el) {
+              return <li><a href={el.route}>{el.name}</a></li>;
+            })
+          }
+        </ul>
+      </div>
     );
   }
 });
