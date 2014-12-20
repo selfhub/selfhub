573e246f350c74688a1c4e410dbc0a51148ddb16
zindlerb <zindlerb@gmail.com>
2014-12-19 19:10:22 -0600

    complete spreadsheet, upload, and download components

---
+++ client/js/components/data_page.jsx
@@ -0,0 +1,93 @@
+var React = require("react");
+var _ = require("underscore");
+var reactBootStrap = require("react-bootstrap");
+
+var UploadButton = React.createClass({
+  fileHandler: function(event) {
+    console.log(this.files);
+  },
+  componentDidMount: function() {
+    var inputElement = document.getElementById("input");
+    inputElement.addEventListener("change", this.fileHandler, false);
+  },
+  render: function() {
+    return (
+      <div>
+        <input type="file" id="input"></input>
+      </div>
+    );
+  }
+});
+
+var DownloadButton = React.createClass({
+  render: function() {
+    return (
+      <div class="formbutton">
+        <form action={this.props.url} method="get">
+          <button type="submit">Download</button>
+        </form>  
+      </div>
+    );
+  }
+});
+
+var TableRows = React.createClass({
+    render: function() {
+      return (
+        <tbody>
+          {
+            _.map(this.props.rowData, function(row) {
+                  return (<tr> 
+                            {
+                              _.map(row, function(rowItem) {
+                                return <td>{rowItem}</td>;
+                              })
+                            }
+                          </tr>);
+            })
+          }
+        </tbody>
+      );    
+    }
+});
+
+var TableHeadersRow = React.createClass({
+    render: function() {
+      return (
+        <tr>
+          {
+            _.map(this.props.headerKeys, function(header) {
+              return <th>{header}</th>;
+            })
+          }
+        </tr>
+      );
+    }
+});
+
+var Table = React.createClass({
+ getInitialState: function() {
+  return {
+    mockFitBitData: [
+      {name: "Brian", miles: 1, month: "feb"},
+      {name: "Thomas", miles: 3.4, month: "march"},
+      {name: "Zindler", miles: 45, month: "july"},
+      {name: "Murphy", miles: 2, month: "jan"}
+    ]
+  };
+ },
+ render: function() {
+   var Table = reactBootStrap.Table;
+   var fitBitData = this.state.mockFitBitData;
+   var headerKeys = Object.keys(fitBitData[0]);
+   
+   return (
+     <Table striped bordered condensed hover>
+        <TableHeadersRow headerKeys={headerKeys}/>
+        <TableRows rowData={fitBitData}/>
+     </Table>
+   );
+ }
+});
+
+module.exports = Table;
