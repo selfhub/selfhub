80418cf1c9671d8e08e7a114c9c6c68e82a8f7b1
owen-d <ow.diehl@gmail.com>
2014-12-29 10:54:19 -0800

    changed userRoute to signin instead of create

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,8 +2,8 @@ var userController = require('./userController');
 
 //we inject a router as the first argument (see: ../config/middleware)
 module.exports = function(router) {
-  router.get('/signin', userController.signin);
-  router.get('/create', userController.signup);
+  router.post('/signin', userController.signin);
+  router.post('/signup', userController.signup);
   router.get('/fetch', userController.getUsers);
   router.get('/remove', userController.dropUsers);
 };
